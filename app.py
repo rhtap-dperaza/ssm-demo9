b520001534e4fd00506bad7b04300f8299d5e572
David Peraza <dperaza@redhat.com>
2025-05-28 16:03:19 -0400

    Update app.py
    Third

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World TSSC good Tekton!"
+    return "Hello World TSSC bad Tekton!"
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
